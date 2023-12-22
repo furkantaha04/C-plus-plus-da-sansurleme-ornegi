a2524b1dda1008864b5bd8b3e2712b52843f0eb4
furkantaha04 <112404222+furkantaha04@users.noreply.github.com>
2023-12-22 15:47:33 +0300

    sansurkeme_ornegi
    ünlü harfleri sansürler

---
+++ SANSÜRLEME ÖRNEĞİ.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+string x;                           //SANSÜRLEME ÖRNEĞİ
+char gecici;
+cout << "lutfen bir kelime giriniz: ";
+cin >> x;
+
+for (  int i = 0; i < x.length(); i++)      // KELİMENİN UZUNLUĞUNU DÖNGÜYE ALIR
+{
+	gecici = x[i];
+
+	if (gecici== 'a' ||gecici ==  'u' ||gecici== 'e' ||gecici== 'A' ||gecici== 'E' ||gecici== 'U' ||gecici== 'i' ||gecici== 'I'||gecici== 'o') { // SESLİ HARF SANSÜRÜ
+
+		x[i] = '*';
+	}
+
+}
+cout<<x<<endl; // EKRANA BASMA
+
+
+}
+
